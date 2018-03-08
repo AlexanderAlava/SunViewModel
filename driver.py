020d5a9069ebcb9ef6d2a9f8692690da08775924
Adrian Borrego <aborrego1@mail.usf.edu>
2018-03-07 22:42:15 -0500

    Updated driver.py with the newest version from python 0.4 files

--- driver.py
+++ driver.py
@@ -1,9 +1,7 @@
-# Original driver #
-
 import fasttext
 query = []
 
-model = fasttext.load_model('model.bin', label_prefix='__label__')
+model = fasttext.load_model('modelMaster.bin', label_prefix='__label__')
 
 print ('Testing the model')
 result = model.test('TestData.txt')
@@ -14,12 +12,21 @@ print 'Number of examples: ', result.nexamples
 score = 2 * ((result.precision * result.recall) / (result.precision + result.recall))
 print 'Accuracy: ', score
 
-while True:
+while True: 
 	query.append(raw_input('What is your query?\n'))
-	if query[0].lower() == 'exit':
+	if query == 'exit':
 		break
-	labels = model.predict_proba(query, k = 3)
+
+	labels = model.predict_proba(query)
+	
+	if (labels[0][0][1] <= 0.60):
+		query.pop(0)
+		query.append(raw_input('Hmm, I do not fully understand. Can you clarify the question please.\n'))
+		if query[0] == 'exit':
+			break
+
+	labels = model.predict_proba(query)	  
 	print labels
-	query.pop()
+	query.pop(0)
 
 print("Goodbye!")
