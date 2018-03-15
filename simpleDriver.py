eb89b91bc64c57d1b0f384544e32e0d6b77798f8
Alexander Alava <alava@mail.usf.edu>
2018-03-15 16:03:02 -0400

    Added responses for two non-tech cases and structure for two other that will be added later today. Changed name of the bucket selection function since it only relates to non-tech buckets.

--- simpleDriver.py
+++ simpleDriver.py
@@ -5,7 +5,7 @@ import time
 from random import *
 
 # "Switch case" for buckets
-def bucketSelector(bucket):
+def bucketSelectorNonTech(bucket):
 	if (bucket == "greetings"):
 		# Print option(s) for greetings
 		print("greetings")
@@ -31,17 +31,41 @@ def bucketSelector(bucket):
 		# Print option(s) for bot
 		print("bot")
 	elif (bucket == "itsm"):
-		# Print option(s) for itsm
-		print("itsm")
+		itsmA = "a"
+		itsmB = "b"
+
+		itsmMessages = [itsmA, itsmB]
+		itsmResponse = sample(itsmMessages, 1)
+
+		print(itsmResponse[0])
+
 	elif (bucket == "halloween"):
-		# Print option(s) for halloween
-		print("halloween")
+		halloweenA = "Spooky!\nLast year I dressed up as human for Halloween, I had a great time but people looked at me like a freak when I talked to anyone. Maybe I was a little bit too robotic?\nIf you wish to know more about Halloween related vacations, please visit the FAQ page for company holidays."
+		halloweenB = "Good friends share their Halloween candy!\nThankfully, I am not a good friend and kept all the candy for myself last year. I remember I dressed as a . . . . chatbot?\nIf you wish to know more about Halloween related vacations, please visit the FAQ page for company holidays."
+
+		halloweenMessages = [halloweenA, halloweenB]
+		halloweenResponse = sample(halloweenMessages, 1)
+
+		print(halloweenResponse[0])
+
 	elif (bucket == "changegear"):
-		# Print option(s) for changegear
-		print("changegear")
+		changegearA = "ChangeGear? Mmmmmm, why does that sound so familiar to me?\nOh! That's right! I am part of it!"
+		changegearB = "Great that you ask me!\nChangeGear and I have been great friends since my creation. We work together, we live together, we do everyhting together!"
+
+		changegearMessages = [changegearA, changegearB]
+		changegearResponse = sample(changegearMessages, 1)
+
+		print(changegearResponse[0])
+		print("ChangeGear products, developed by SunView Software, deliver intelligent, people-centric IT service management with critical enterprise-grade features you need to ensure that your solution is able to meet your current and future business requirements.\nIf you would like more information about the different available products, please refer to SunView's Software website at https://www.sunviewsoftware.com/products")
+
 	elif (bucket == "sst"):
-		# Print option(s) for sst
-		print("sst")
+		sstA = "a"
+		sstB = "b"
+
+		sstMessages = [sstA, sstB]
+		sstResponse = sample(sstMessages, 1)
+
+		print(sstResponse[0])
 
 
 ################################# MAIN #########################################
@@ -96,7 +120,7 @@ while True:
 	else:
 		keyword = query[0] # This should be the first label
 		print("Con > 0.6")
-		bucketSelector(query[0]) # "Switch case" for buckets
+		bucketSelectorNonTech(query[0]) # "Switch case" for buckets
 
 		# Resetting flag
 		flag = 0
