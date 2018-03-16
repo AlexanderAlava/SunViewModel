087fdf6e7572cdf10a092948596973683b57dbc5
brianuz <brianuzan@gmail.com>
2018-03-16 15:03:47 -0400

    added technical query answer

--- simpleDriver.py
+++ simpleDriver.py
@@ -135,7 +135,7 @@ def bucketSelectorNonTech(bucket):
 		print("If you would like to learn more about SST, please visit https://www.sunviewsoftware.com/solutions/machine-learning-for-itsm and make sure to watch the video linked in there!")
 
 	else:
-		print "Here is a FAQ page for help with " + bucket
+		print "Here is a FAQ page for help with " + bucket 
 	
 
 ################################# MAIN #########################################
